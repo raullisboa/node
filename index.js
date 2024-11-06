b7f53bce6b2878ceb1fb262d01795288f8dec8d6
Raul Dev <raullisboasilva@gmail.com>
2024-11-05 21:36:34 -0300

    part -- frontend

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("Hello, Express!");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/note/:id", (req, res) => {
+  res.sendFile(__dirname + "/public/note.html");
 });
 
 const PORT = 3000;
